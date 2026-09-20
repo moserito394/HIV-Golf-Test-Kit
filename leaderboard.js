16e45a390bf27f63961e41cad279363bd6ca81ad
moserito394 <rjmoser03@gmail.com>
2026-09-19 22:32:49 -0400

    Update Supabase credentials in leaderboard.js
    Updated Supabase URL and anon key for leaderboard.

--- leaderboard.js
+++ leaderboard.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://edhnlhbmmztvflpjhwga.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkaG5saGJtbXp0dmZscGpod2dhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4NTc2MzAsImV4cCI6MjEwNTQzMzYzMH0.53CN7eiQ1q8-3KKQg6P0ckieVdKh-gQP4uiwn0ukSks";
 
 
 // -------------------------
